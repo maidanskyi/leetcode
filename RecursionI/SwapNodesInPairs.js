d95fecfabe4af9b40ac5b7e87f9d4b5484974ba7
Roman Maidanskyi <rmaidanskyy@prismhr.com>
2019-12-21 17:48:37 +0200

    feat: solved problem №24 Swap Nodes in Pairs

---
+++ RecursionI/SwapNodesInPairs.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const swapPairs = head => {
+
+  if (!head)
+    return null;
+
+  if (!head.next)
+    return head;
+
+  const c = head.next.next;
+  const b = head.next;
+  b.next = head;
+  b.next.next = swapPairs(c);
+  return b;
+
+}
+
+const list = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 4,
+        next: {
+          val: 5,
+          next: {
+            val: 6,
+            next: null,
+          },
+        },
+      },
+    },
+  },
+}
+
+console.log(JSON.stringify(swapPairs(list)));
