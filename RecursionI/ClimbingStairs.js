7630be7fb3cdf004d43773589a47508f171d1c03
Roman Maidanskyi <rmaidanskyy@prismhr.com>
2019-12-22 13:07:48 +0200

    refactor: solved problem #70 Climbing Stairs

--- RecursionI/ClimbingStairs.js
+++ RecursionI/ClimbingStairs.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 const climbStairs = n => {
-  const hash = [0, 1, 2]
-  for (let i = 3; i < n + 1; i++) {
+  const hash = [1, 2]
+  for (let i = 2; i < n; i++) {
     hash[i] = hash[i - 1] + hash[i - 2]
   }
-  return hash[n]
+  return hash[n-1]
 }
