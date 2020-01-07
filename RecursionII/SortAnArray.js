8bd3661b5c2876541e31d5587a246951383a10ee
Roman Maidanskyi <rmaidanskyy@prismhr.com>
2020-01-07 14:06:39 +0200

    feat: improve #912 Sort An Array

--- RecursionII/SortAnArray.js
+++ RecursionII/SortAnArray.js
@@ -5,7 +5,29 @@
 const sortArray = nums => {
   if (nums.length === 1) return nums;
 
-  let splitted = nums.map(el => [el]);
+  let sortedArray = [];
+  const length = nums.length;
+
+  for (let i = 0; i < length; i += 2) {
+    const arr = [];
+
+    if ( i + 1 === length) {
+      arr.push(nums[i]);
+    } else {
+
+      if (nums[i] < nums[i + 1]) {
+        arr.push(nums[i]);
+        arr.push(nums[i + 1]);
+      } else {
+        arr.push(nums[i + 1]);
+        arr.push(nums[i]);
+      }
+
+
+    }
+
+    sortedArray.push(arr);
+  }
 
   const merge = (left = [], right = []) => {
     const arr = [];
@@ -33,20 +55,20 @@ const sortArray = nums => {
     return arr;
   }
 
-  while (splitted.length !== 1) {
+  while (sortedArray.length !== 1) {
 
     const arr = [];
 
-    for (let i = 0; i < splitted.length; i += 2) {
-      arr.push(merge(splitted[i], splitted[i+1]));
+    for (let i = 0; i < sortedArray.length; i += 2) {
+      arr.push(merge(sortedArray[i], sortedArray[i + 1]));
     }
 
-    splitted = arr;
+    sortedArray = arr;
 
   }
 
-  return splitted[0];
+  return sortedArray[0];
 
 };
 
-console.log(sortArray([4, 6, 9, 3, 1, 2, 6, 5, 3, 9, -1, -3434, 0, 100]));
+console.log(sortArray([4, 9, 3, 1, 2, 6, 5, 3, 9, -1, -3434, 0, 100]));
