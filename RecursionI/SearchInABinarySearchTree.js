492215fe75c1d74f8b1a46088e0a8e40d26e8ad3
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-04-03 12:56:44 +0300

    feat: #700 Search in a Binary Search Tree

--- RecursionI/SearchInABinarySearchTree.js
+++ RecursionI/SearchInABinarySearchTree.js
@@ -14,18 +14,9 @@ const searchBST = (root, val) => {
 
   if (!root) return null;
 
-  const fn = (node) => {
-    if (!node) return node;
-
-    if (node.val === val) return node;
-
-    if (node.val > val) return fn(node.left);
-    if (node.val < val) return fn(node.right);
-  }
-
   if (root.val === val) return root;
-  if (root.val > val) return fn(root.left);
-  if (root.val < val) return fn(root.right);
+  if (root.val > val) return searchBST(root.left, val);
+  if (root.val < val) return searchBST(root.right, val);
 
 }
 
