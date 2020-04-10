df24332cc87543be2eab1adbeb61c3feedfa3251
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-04-10 16:02:08 +0300

    feat: #344 Reverse String

--- RecursionI/ReverseString.js
+++ RecursionI/ReverseString.js
@@ -1,5 +1,23 @@
 /**
- * @param {character[]} s
+ * @param {string[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-const reverseString = s => s.reverse();
+const reverseString = s => {
+
+  let i = 0;
+  let j = s.length - 1;
+
+  while (i < j) {
+    let temporary = s[i];
+    s[i] = s[j];
+    s[j] = temporary;
+    i++;
+    j--;
+  }
+
+  return s;
+
+}
+
+console.log(reverseString(['h', 'e', 'l', 'l', 'o'])); // 'olleh'
+
