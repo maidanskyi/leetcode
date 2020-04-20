e48a607a597bddc76c52e9e0a3664f641576dfc0
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-04-20 22:56:51 +0300

    feat: #328 Odd Even Linked List

---
+++ LinkedList/328-OddEvenLinkedList.js
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const oddEvenList = (head) => {
+
+  if (!head || !head.next || !head.next.next) return head;
+
+  let lastOdd = head;
+  let firstEven = head.next;
+  let lastEven = head.next;
+
+  while (lastEven.next && lastEven.next.next) {
+    const currOdd = lastEven.next;
+    const currEven = lastEven.next.next;
+    lastOdd.next = currOdd;
+    lastEven.next = currEven;
+    lastOdd = currOdd;
+    lastEven = currEven;
+  }
+
+  if (!lastEven.next) {
+    lastOdd.next = firstEven;
+  } else {
+    lastOdd.next = lastEven.next;
+    lastEven.next = null;
+    lastOdd.next.next = firstEven;
+  }
+
+  return head;
+
+}
+
+console.log(JSON.stringify(oddEvenList({
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 4,
+        next: {
+          val: 5,
+          next: {
+            val: 6,
+            next: null,
+          },
+        },
+      },
+    },
+  },
+})));
