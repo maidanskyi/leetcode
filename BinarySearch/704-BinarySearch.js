66f3a871cd288d118c7c5cfcecb5ba232142b8ae
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-04-27 10:16:26 +0300

    feat: #704 Binary Search

---
+++ BinarySearch/704-BinarySearch.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = (nums, target) => {
+
+  if (!nums.length) return -1;
+  if (nums.length === 1 && nums[0] !== target) return -1;
+
+  let from = 0;
+  let to = nums.length;
+
+  while (to - from) {
+    let middle = Math.floor((to + from) / 2);
+    if (target === nums[middle]) return middle;
+    if (target < nums[middle]) {
+      to = middle;
+      continue;
+    }
+    from = middle + 1;
+  }
+
+  return -1;
+
+}
+
+console.log(search([-1, 0], -2));
