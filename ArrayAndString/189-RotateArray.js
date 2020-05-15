0e7c7028d51c3933ddd89d46f08625f82fb3646d
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-05-15 12:16:30 +0300

    feat: #189 Rotate Array

---
+++ ArrayAndString/189-RotateArray.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const rotate = (nums, k) => {
+
+  if (!nums.length || nums.length === k || !(k % nums.length)) return;
+  if (nums.length > k) {
+    k > nums.length / 2
+      ? rotateFromLeftToRight(nums, nums.length - k)
+      : rotateFromRightToLeft(nums, k);
+  } else {
+    k = k % nums.length;
+    k > nums.length / 2
+      ? rotateFromLeftToRight(nums, nums.length - k)
+      : rotateFromRightToLeft(nums, k);
+  }
+
+}
+
+const rotateFromLeftToRight = (nums, k) => {
+  console.log('left --> right');
+  while (k) {
+    const temp = nums[0];
+    for (let i = 0; i < nums.length; i++) {
+      nums[i] = nums[i + 1];
+    }
+    nums[nums.length - 1] = temp;
+    k--;
+  }
+}
+const rotateFromRightToLeft = (nums, k) => {
+  console.log('right --> left');
+  while (k) {
+    const temp = nums[nums.length - 1];
+    for (let i = nums.length - 1; i > 0; i--) {
+      nums[i] = nums[i - 1];
+    }
+    nums[0] = temp;
+    k--;
+  }
+}
+
+const arr = [1,2,3,4,5,6,7];
+rotate(arr, 13);
+console.log(arr);
