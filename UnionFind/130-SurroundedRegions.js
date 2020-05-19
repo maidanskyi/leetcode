b746167cac98adbc749de34292494900bf9b1aac
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-05-19 10:58:16 +0300

    feat: #130 Surrounded Regions (DFS solution)

--- UnionFind/130-SurroundedRegions.js
+++ UnionFind/130-SurroundedRegions.js
@@ -7,16 +7,14 @@ const solve = (board) => {
   if (board.length <= 2 || board[0].length <= 2) return;
 
   const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-  const onBorder = new Set();
   const dfs = (i = 0, j = 0) => {
     if (
       !board[i]
       || !board[i][j]
       || board[i][j] !== 'O'
-      || onBorder.has(`i${i}j${j}`)
     ) return;
 
-    onBorder.add(`i${i}j${j}`);
+    board[i][j] = 'M';
 
     for (const [x, y] of directions) {
       dfs(i + x, j + y);
@@ -40,14 +38,13 @@ const solve = (board) => {
     if (board[i][board[i].length - 1] === 'O') dfs(i, board[i].length - 1);
   }
 
-  console.log(onBorder);
-
   // change not connected
-  for (let i = 1; i < board.length - 1; i++) {
-    for (let j = 1; j < board[0].length - 1; j++) {
-      if (
-        board[i][j] === 'O' && !onBorder.has(`i${i}j${j}`)
-      ) board[i][j] = 'X';
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] === 'O')
+        board[i][j] = 'X';
+      else if (board[i][j] === 'M')
+        board[i][j] = 'O';
     }
   }
 
