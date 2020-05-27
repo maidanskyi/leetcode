d0c960ace1960df154139e3b7626a103f5658615
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-05-27 10:30:15 +0300

    feat: #215 Kth Largest Element in an Array (QuickSelection Solution)

--- Others/215-KthLargestElementInAnArray.js
+++ Others/215-KthLargestElementInAnArray.js
@@ -5,11 +5,45 @@
  */
 const findKthLargest = (nums, k) => {
 
-  nums.sort((a, b) => b - a);
+  const exchange = (i, j) => {
+    const temp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = temp;
+  }
 
-  return nums[k - 1];
+  const partition = (start, end) => {
+    let pivot = end;
+    let wall = start;
+
+    for (let i = start; i < end; i++) {
+      if (nums[i] < nums[pivot]) {
+        exchange(i, wall);
+        wall++;
+      }
+    }
+    exchange(wall, pivot);
+
+    return wall;
+  }
+
+  const quickSelect = (searchPosition, start = 0, end = nums.length - 1) => {
+
+    if (start > end) return;
+
+    const pivot = partition(start, end);
+
+    if (pivot === searchPosition) return nums[pivot];
+    if (pivot > searchPosition)
+      return quickSelect(searchPosition, start, pivot - 1);
+
+    return quickSelect(searchPosition, pivot + 1, end);
+
+  }
+
+  return quickSelect(nums.length - k);
 
 }
 
 console.log(findKthLargest([3,2,1,5,6,4], 2)); // 5
 console.log(findKthLargest([3,2,3,1,2,4,5,5,6], 4)); // 4
+console.log(findKthLargest([1], 1)); // 4
