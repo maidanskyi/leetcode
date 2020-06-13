75cfd8342351bd731a6097550722034a0a55ea5d
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-06-13 22:14:24 +0300

    refactor: #354 Russian Doll Envelopes

--- Others/354-RussianDollEnvelopes.js
+++ Others/354-RussianDollEnvelopes.js
@@ -4,7 +4,7 @@
  */
 const maxEnvelopes = (envelopes) => {
 
-  if (!envelopes.length) return 0;
+  if (envelopes.length < 2) return envelopes.length;
 
   envelopes.sort(([w1,h1], [w2,h2]) => {
     if (w1 - w2 === 0)
@@ -17,18 +17,17 @@ const maxEnvelopes = (envelopes) => {
   const hash = new Array(envelopes.length).fill(1);
 
   for (let i = 1; i < envelopes.length; i++) {
-    const values = [1];
     for (let j = i - 1; j >=0; j--) {
       if (containsDoll(envelopes[i], envelopes[j])) {
-        values.push(hash[i] + hash[j]);
+        hash[i] = Math.max(hash[i], hash[j] + 1);
       }
     }
-    hash[i] = Math.max.apply(null, values);
   }
 
   return Math.max.apply(null, hash);
 
 }
 
-// console.log(maxEnvelopes([[5,4],[6,4],[6,7],[2,3]])); // 3
+console.log(maxEnvelopes([[5,4],[6,4],[6,7],[2,3]])); // 3
 console.log(maxEnvelopes([[46,89],[50,53],[52,68],[72,45],[77,81]])); // 3
+console.log(maxEnvelopes([[1,1]])); // 1
