4cab1c7084fbb40850cb12c9d29f696abc00668e
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-06-14 20:29:55 +0300

    feat: #746 Min Cost Climbing Stairs

---
+++ Others/746-MinCostClimbingStairs.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+const minCostClimbingStairs = (cost) => {
+
+  for (let i = 2; i < cost.length; i++) {
+    cost[i] = Math.min(cost[i - 1], cost[i - 2]) + cost[i];
+  }
+
+  return Math.min(cost[cost.length - 1], cost[cost.length - 2]);
+
+}
+
+console.log(minCostClimbingStairs([10, 15, 20])); // 15
+console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])); // 6
