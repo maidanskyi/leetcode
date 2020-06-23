d63df2a963c541f05202513c71d48047e5125e94
Roman Maidanskyi <maidanskiy.roman@gmail.com>
2020-06-23 11:05:50 +0300

    feat: #222 Count Complete Tree Nodes (Inorder Traversal)

---
+++ Others/222-CountCompleteTreeNodes.js
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+const countNodes = (root) => {
+
+  let response = 0;
+  const traverse = node => {
+    if (!node) return;
+
+    traverse(node.left);
+    response += 1;
+    traverse(node.right);
+
+  }
+
+  traverse(root);
+
+  return response;
+}
+
+console.log(countNodes({
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 5,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 3,
+    left: {
+      val: 6,
+      left: null,
+      right: null,
+    },
+    right: null,
+  },
+}));
